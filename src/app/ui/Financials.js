ffdeb8b2962feba78d5c3d0cbe05f603a944cbdb
Tim <timkok99@gmail.com>
2025-01-09 19:47:21 +1100

    working on 5 year growth rate

--- src/app/ui/Financials.js
+++ src/app/ui/Financials.js
@@ -8,9 +8,12 @@ let currency = "USD";
 export default function Financials() {
   const [FreeCashFlowEquityData, setFreeCashFlowEquityData] = useState([]);
   const [netIncomeData, setNetIncomeData] = useState([]);
-  const [depreceationAmortizationData, setDepreceationAmortizationData] = useState([]);
+  const [depreceationAmortizationData, setDepreceationAmortizationData] =
+    useState([]);
   const [capitalExpenditureData, setCapitalExpenditureData] = useState([]);
-  const [changeInWorkingCapitalData, setChangeInWorkingCapitalData] = useState([]);
+  const [changeInWorkingCapitalData, setChangeInWorkingCapitalData] = useState(
+    []
+  );
   const [netBorrowingData, setNetBorrowingData] = useState(null);
   const [fiveYearGrowthRate, setFiveYearGrowthRate] = useState([]);
 
@@ -87,22 +90,44 @@ export default function Financials() {
     };
 
 
+    //finished API limits + inaccurate data for some searches - looking for alternatives
     const fetchFiveYearGrowthRate = async () => {
-        try{
-            const response = await axios.get("https://www.alphavantage.co/query?function=EARNINGS&symbol=AAPL&apikey=496Z5WWYIJYB3MFM"
-
-            );
-
-            const data = response.data.annualEarnings 
-            console.log({data});
-
-
-            
+      try {
+        const response = await axios.get(
+          "https://www.alphavantage.co/query?function=EARNINGS&symbol=IBM&apikey=496Z5WWYIJYB3MFM"
+        );
+    
+        const data = response.data.annualEarnings;
+        console.log("Annual Earnings Data:", data);
+    
+        // Check if the data contains at least 5 years of EPS data
+        if (data.length >= 5) {
+          const mostRecentEPS = parseFloat(data[0].reportedEPS); // Most recent year
+          const fiveYearsAgoEPS = parseFloat(data[4].reportedEPS); // 5 years ago
+    
+          console.log("Most Recent EPS:", mostRecentEPS);
+          console.log("5 Years Ago EPS:", fiveYearsAgoEPS);
+    
+          // Ensure the EPS values are valid
+          if (!isNaN(mostRecentEPS) && !isNaN(fiveYearsAgoEPS) && fiveYearsAgoEPS > 0) {
+            // Calculate CAGR
+            const growthRate =
+              Math.pow(mostRecentEPS / fiveYearsAgoEPS, 1 / 5) - 1;
+            console.log("5-Year EPS Growth Rate:", growthRate * 100);
+    
+            setFiveYearGrowthRate((growthRate * 100).toFixed(2)); // Store as percentage
+          } else {
+            console.error("Invalid EPS values:", { mostRecentEPS, fiveYearsAgoEPS });
+            setFiveYearGrowthRate("Invalid data");
+          }
+        } else {
+          console.error("Not enough data for 5 years.");
+          setFiveYearGrowthRate("Insufficient data");
         }
-        catch(error){
-            console.error("Error fetching 5 year growth rate")
-        }
-    }
+      } catch (error) {
+        console.error("Error fetching 5-year growth rate:", error);
+      }
+    };
 
     fetchNetIncome();
     fetchCashFlow();
@@ -133,115 +158,126 @@ export default function Financials() {
     netBorrowingData,
   ]);
 
-
   return (
-    <div className="fcfe-container">
-      <div className="fcfe-row">
-        <span className="fcfe-label">Free Cash Flow to Equity</span>
-        <span className="fcfe-value">
-          <div className="price-qty">
-            <span className="currency">{currency}</span>
-            <span className="number-inner">
-              ${" "}
-              {FreeCashFlowEquityData !== null
-                ? FreeCashFlowEquityData.toLocaleString()
-                : "Calculating..."}
+    <>
+      <div className="fcfe-container">
+        <div className="fcfe-row">
+          <span className="fcfe-label">Free Cash Flow to Equity</span>
+          <span className="fcfe-value">
+            <div className="price-qty">
+              <span className="currency">{currency}</span>
+              <span className="number-inner">
+                ${" "}
+                {FreeCashFlowEquityData !== null
+                  ? FreeCashFlowEquityData.toLocaleString()
+                  : "Calculating..."}
+              </span>
+            </div>
+          </span>
+        </div>
+
+        <div className="fcfe-values">
+          <div className="fcfe-row">
+            <span className="fcfe-label">Net Income</span>
+            <span className="fcfe-value">
+              <div className="price-qty-inner">
+                <div className="currency-inner">
+                  <p>{currency}</p>
+                </div>
+                <div className="number-inner">
+                  <p>
+                    {netIncomeData.length > 0
+                      ? netIncomeData[0].netIncome.toLocaleString()
+                      : "Loading..."}
+                  </p>
+                </div>
+              </div>
             </span>
           </div>
+
+          <div className="fcfe-row">
+            <span className="fcfe-label">Depreciation & Amortization</span>
+            <span className="fcfe-value">
+              <div className="price-qty-inner">
+                <div className="currency-inner">
+                  <p>{currency}</p>
+                </div>
+                <div className="number-inner">
+                  <p>
+                    {depreceationAmortizationData.length > 0
+                      ? depreceationAmortizationData[0].depreceationAmortization.toLocaleString()
+                      : "Loading..."}
+                  </p>
+                </div>
+              </div>
+            </span>
+          </div>
+
+          <div className="fcfe-row">
+            <span className="fcfe-label">Capital Expenditure</span>
+            <span className="fcfe-value">
+              <div className="price-qty-inner">
+                <div className="currency-inner">
+                  <p>{currency}</p>
+                </div>
+                <div className="number-inner">
+                  <p>
+                    {capitalExpenditureData.length > 0
+                      ? capitalExpenditureData[0].capitalExpenditure.toLocaleString()
+                      : "Loading..."}
+                  </p>
+                </div>
+              </div>
+            </span>
+          </div>
+
+          <div className="fcfe-row">
+            <span className="fcfe-label">Change In Working Capital</span>
+            <span className="fcfe-value">
+              <div className="price-qty-inner">
+                <div className="currency-inner">
+                  <p>{currency}</p>
+                </div>
+                <div className="number-inner">
+                  <p>
+                    {changeInWorkingCapitalData.length > 0
+                      ? changeInWorkingCapitalData[0].changeInWorkingCapital.toLocaleString()
+                      : "Loading..."}
+                  </p>
+                </div>
+              </div>
+            </span>
+          </div>
+
+          <div className="fcfe-row">
+            <span className="fcfe-label">Net Borrowing</span>
+            <span className="fcfe-value">
+              <div className="price-qty-inner">
+                <div className="currency-inner">
+                  <p>{currency}</p>
+                </div>
+                <div className="number-inner">
+                  <p>
+                    {netBorrowingData !== null
+                      ? netBorrowingData.toLocaleString()
+                      : "Loading..."}
+                  </p>
+                </div>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      
+        <span className="fcfe-label">5-Year EPS Growth Rate:</span>
+        <span className="fcfe-value">
+          {fiveYearGrowthRate !== "Invalid data" &&
+          fiveYearGrowthRate !== "Insufficient data"
+            ? `${fiveYearGrowthRate}%`
+            : fiveYearGrowthRate}
         </span>
-      </div>
-
-      <div className="fcfe-values">
-        <div className="fcfe-row">
-          <span className="fcfe-label">Net Income</span>
-          <span className="fcfe-value">
-            <div className="price-qty-inner">
-              <div className="currency-inner">
-                <p>{currency}</p>
-              </div>
-              <div className="number-inner">
-                <p>
-                  {netIncomeData.length > 0
-                    ? netIncomeData[0].netIncome.toLocaleString()
-                    : "Loading..."}
-                </p>
-              </div>
-            </div>
-          </span>
-        </div>
-
-        <div className="fcfe-row">
-          <span className="fcfe-label">Depreciation & Amortization</span>
-          <span className="fcfe-value">
-            <div className="price-qty-inner">
-              <div className="currency-inner">
-                <p>{currency}</p>
-              </div>
-              <div className="number-inner">
-                <p>
-                  {depreceationAmortizationData.length > 0
-                    ? depreceationAmortizationData[0].depreceationAmortization.toLocaleString()
-                    : "Loading..."}
-                </p>
-              </div>
-            </div>
-          </span>
-        </div>
-
-        <div className="fcfe-row">
-          <span className="fcfe-label">Capital Expenditure</span>
-          <span className="fcfe-value">
-            <div className="price-qty-inner">
-              <div className="currency-inner">
-                <p>{currency}</p>
-              </div>
-              <div className="number-inner">
-                <p>
-                  {capitalExpenditureData.length > 0
-                    ? capitalExpenditureData[0].capitalExpenditure.toLocaleString()
-                    : "Loading..."}
-                </p>
-              </div>
-            </div>
-          </span>
-        </div>
-
-        <div className="fcfe-row">
-          <span className="fcfe-label">Change In Working Capital</span>
-          <span className="fcfe-value">
-            <div className="price-qty-inner">
-              <div className="currency-inner">
-                <p>{currency}</p>
-              </div>
-              <div className="number-inner">
-                <p>
-                  {changeInWorkingCapitalData.length > 0
-                    ? changeInWorkingCapitalData[0].changeInWorkingCapital.toLocaleString()
-                    : "Loading..."}
-                </p>
-              </div>
-            </div>
-          </span>
-        </div>
-
-        <div className="fcfe-row">
-          <span className="fcfe-label">Net Borrowing</span>
-          <span className="fcfe-value">
-            <div className="price-qty-inner">
-              <div className="currency-inner">
-                <p>{currency}</p>
-              </div>
-              <div className="number-inner">
-                <p>
-                  {netBorrowingData !== null
-                    ? netBorrowingData.toLocaleString()
-                    : "Loading..."}
-                </p>
-              </div>
-            </div>
-          </span>
-        </div>
-      </div>
-    </div>
+   
+    </>
   );
 }
